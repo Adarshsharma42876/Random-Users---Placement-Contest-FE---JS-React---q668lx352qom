8fa27fddc6a7ecf956da493e7f9c51d09fd58c2a
ADARSH SHARMA <120009672+Adarshsharma42876@users.noreply.github.com>
2023-09-03 18:56:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,13 @@ const App = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+            setLoading(true);
+            fetch('https://randomuser.me/api/?results=10')
+            .then((res)=>res.json())
+            .then((data)=>{setItems(data.results)});
+            setLoading(false);
+    }, []);
 
     return <Users isLoading={isLoading} items={items} />;
 };
